04d9ef8bb34b763138643c986136506ea7e41b47
kelvinpinoy <kelvin.lapidario@gmail.com>
2012-11-02 20:36:56 -0700

    set admin default

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :address, :phone, :admin, :email, :birthday, :username, :password
+  before_save :default_values
+  
+  def default_values
+    self.admin ||= 0
+  end
+
 end

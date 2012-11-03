7b445498fd20ef818b2d7f53e98f10af13030d09
kelvinpinoy <kelvin.lapidario@gmail.com>
2012-11-02 20:50:15 -0700

    added all defaults

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,16 @@ class User < ActiveRecord::Base
   before_save :default_values
   
   def default_values
+    self.username ||= ""
+    self.first_name ||= ""
+    self.last_name ||= ""
+    self.address ||= ""
+    self.email ||= ""
+    self.password ||= "ConMan"
     self.admin ||= 0
+    self.phone ||= 1234567890
+    self.birthday ||= 11/11/11
+    
   end
 
 end

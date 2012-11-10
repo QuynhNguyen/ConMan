5e5d7260677be3d2181664ce11056e980733988d
mito314 <mito314@yahoo.com>
2012-11-10 15:31:44 -0800

    create and show private messages

--- app/controllers/private_messages_controller.rb
+++ app/controllers/private_messages_controller.rb
@@ -2,12 +2,34 @@ class PrivateMessagesController < ApplicationController
 	def index
 		@pm = PrivateMessage.all
 	end
+
+	def show
+		@private_message = PrivateMessage.find(params[:id])
+	end
 	
 	def new
 		#ignore
 	end
 	
 	def create
-		@private_message = PrivateMessage.create(params[:private_message])
+		@user = User.where("username = '#{params[:private_message][:user]}'").first
+		#@private_message = PrivateMessage.create(message: params[:private_message][:message], subject: params[:private_message][:subject], user: @user.id)
+		flash[:notice] = "Message was successfully sent!"
+		redirect_to private_messages_path
+	end
+
+	def edit
+		#ignore
+	end
+
+	def update
+		#ignore
+	end
+
+	def destroy
+		@private_message = PrivateMessage.find(params[:id])
+		@private_message.destroy
+		flash[:notice] = "Message deleted."
+		redirect_to private_messages_path
 	end
 end

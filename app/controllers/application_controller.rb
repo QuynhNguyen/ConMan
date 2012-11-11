83a22b56dc863688dc410878673f2c86c17a0d60
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-11-10 20:04:01 -0800

    Add 404 error view and redirect user to 404 page if try to visit non existent profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render_404
+  end
+
+  def render_404
+    respond_to do |format|
+      format.html { render "errors/404", :status => '404 Not Found', :layout => false }
+      format.xml  { render :nothing => true, :status => '404 Not Found' }
+    end
+  end
 end

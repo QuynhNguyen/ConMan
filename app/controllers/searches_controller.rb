5de0e6dba589cd33ef4375cc4950f8a37721cdde
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-11-10 22:07:47 -0800

    added and customized search result

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,5 @@
 class SearchesController < ApplicationController
+	def index
+   		@result = User.where('first_name LIKE ? OR username LIKE ?', "%#{params[:query]}%", "%#{params[:query]}%").all
+	end
 end

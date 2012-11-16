0123a8c3beb7696e88780ce0fea1da18f6ca641a
kelvinpinoy <kelvin.lapidario@gmail.com>
2012-11-15 22:19:25 -0800

    partially working log in/out

--- app/controllers/log_in_controller.rb
+++ app/controllers/log_in_controller.rb
@@ -2,12 +2,14 @@ class LogInController < ApplicationController
 
   skip_filter :login
 
+  def show
+    redirect_to log_in_index_path
+  end
+
   def index
-    @Users = User.all
   end
 
   def create
-#@user = User.where(:username => params[:user][:username], :password => Digest::SHA512.hexdigest(params[:user][:password])).first
     @user = User.authenticate(params[:user][:username], params[:user][:password])
     if @user
       session[:id] = @user.id
@@ -20,6 +22,12 @@ class LogInController < ApplicationController
       flash[:notice] = "Username or Password don't match with our database"
       redirect_to log_in_index_path
     end
+  end
+
+  def destory
+    session[:id] = nil
+    reset_session
+    redirect_to log_in_index_path
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 users = User.create!([	{username: 'testuser1', first_name: 'first1', last_name: 'last1', address: 'address1', email: 'email1@email.email'},
 						{username: 'testuser2', first_name: 'first2', last_name: 'last2', address: 'address2', email: 'email2@email.email'},
+            {username: 'kelvin', password: User.encrypt('kelvin'), first_name: 'kelvin', last_name: 'kelvin', address: 'kelvin', email: 'kelvin@kelvin.com', phone: '8586101411'},
 						{username: 'quynh', password: 'quynh', first_name: 'first2', last_name: 'last2', address: 'address2', email: 'email2@email.email'}])
 
 PrivateMessage.create([	{from: users[0].id, message: 'user1 message1', date: DateTime.now, read: false, user: users[1].id, subject: 'subject1'},

d7db560ed318511f478a69274eaa50e9ced34cc0
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-11-23 10:20:55 -0800

    fixing kel and yos datetime crap

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(:version => 20121112034939) do
     t.integer  "show_addr"
     t.integer  "show_phone"
     t.integer  "show_email"
-    t.integer  "show_birthday"
+    t.datetime  "show_birthday"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
   end

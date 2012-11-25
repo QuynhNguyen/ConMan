fde14de58a1fc3a079fb0a0f1bcaaf9022019270
minh ta <snypergtr@gmail.com>
2012-11-25 14:18:01 -0800

    added logout

--- app/controllers/frontpage_controller.rb
+++ app/controllers/frontpage_controller.rb
@@ -3,15 +3,18 @@ class FrontpageController < ApplicationController
   skip_filter :login
 
   def index
-
+    if(session[:id])
+      redirect_to controller: :profiles, action: :index, id: session[:id]
+    end
   end
 
   def log_out
-  	reset session
+    reset_session
   	redirect_to action: :index
   end
 
   def show
+    reset_session
   	redirect_to action: :index
   end
 

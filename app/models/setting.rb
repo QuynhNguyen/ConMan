52e83bb8ff2b87271240b9ade83a86491e5ca2ff
Quynh Nguyen <quynhnguyen003@gmail.com>
2012-12-05 20:39:56 -0800

    add a fix to settings.rb

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,4 +1,4 @@
 class Setting < ActiveRecord::Base
-  attr_accessible :fb_token, :google_code, :twitter_token, :user_id
+  attr_accessible :fb_token, :google_code, :twitter_token, :user_id, :twitter_secret
   validate :user_id, uniqueness: :true
 end
